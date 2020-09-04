d4f4204743a0f5e73aee6a7b1e59ffd5ea4bc7e8
JigsawPlayer <kulkarniravindra@outlook.com>
2020-09-04 15:16:13 +0000

    updated contactservice class

--- vaadin-crm/src/main/java/com/vaadin/tutorial/crm/backend/entity/AbstractEntity.java
+++ vaadin-crm/src/main/java/com/vaadin/tutorial/crm/backend/entity/AbstractEntity.java
@@ -4,36 +4,37 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 @MappedSuperclass
-public abstract class AbstractEntity {
+public abstract class AbstractEntity{
     @Id
     @GeneratedValue(strategy= GenerationType.SEQUENCE)
     private Long id;
-    public Long getId() {
+    public Long getId(){
         return id;
     }
-    public boolean isPersisted() {
+    
+    public boolean isPersisted(){
         return id != null;
     }
     @Override
-    public int hashCode() {
-        if (getId() != null) {
+    public int hashCode(){
+        if(getId() !=null){
             return getId().hashCode();
         }
         return super.hashCode();
     }
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
+    public boolean equals(Object obj){
+        if(this==obj){
             return true;
         }
-        if (obj == null) {
+        if(obj==null){
             return false;
         }
-        if (getClass() != obj.getClass()) {
+        if(getClass() != obj.getClass()){
             return false;
         }
-        AbstractEntity other = (AbstractEntity) obj;
-        if (getId() == null || other.getId() == null) {
+        AbstractEntity other=(AbstractEntity) obj;
+        if(getId()==null || other.getId()==null){
             return false;
         }
         return getId().equals(other.getId());
