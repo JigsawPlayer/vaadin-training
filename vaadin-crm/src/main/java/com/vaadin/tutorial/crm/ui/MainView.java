7439903a8f1a84e3b7e13b28ed5e359d757d8b19
JigsawPlayer <kulkarniravindra@outlook.com>
2020-09-04 16:05:16 +0000

    commit

--- vaadin-crm/src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ vaadin-crm/src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -1,6 +1,9 @@
 package com.vaadin.tutorial.crm.ui;
+
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import com.vaadin.tutorial.crm.backend.entity.Contact;
 import com.vaadin.tutorial.crm.backend.service.ContactService;
@@ -9,12 +12,14 @@ import org.hibernate.dialect.lock.UpdateLockingStrategy;
 
 @Route("")
 public class MainView extends VerticalLayout {
-private ContactService contactService;
+    private ContactService contactService;
     private Grid<Contact> grid=new Grid<>(Contact.class);
+    private TextField filterText=new TextField();
     public MainView(ContactService contactService) {
         this.contactService=contactService;
         addClassName("list-view");
         setSizeFull();
+        configureFilter();
         configureGrid();
         add(grid);
         updateList();
@@ -23,11 +28,17 @@ private ContactService contactService;
         grid.addClassName("contact-grid");
         grid.setSizeFull();
         grid.setColumns("firstName","lastName","email","status");
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+    }
+    private void configureFilter(){
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e -> updateList());
     }
 
-
     private void updateList(){
-        System.out.println(contactService.findAll());
+        //System.out.println(contactService.findAll());
         grid.setItems(contactService.findAll());
     }
 }
